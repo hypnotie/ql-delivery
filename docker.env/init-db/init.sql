1f414d13db4c4175bb6a936c45ed536034dd6ff7
hypnotie <maksymhanhalo@gmail.com>
2022-07-15 13:44:29 +0300

    Update init.sql

--- docker.env/init-db/init.sql
+++ docker.env/init-db/init.sql
@@ -34,12 +34,12 @@ CREATE TABLE IF NOT EXISTS order_items
 
 INSERT INTO shops (name, logo)
 VALUES 
-('McDonald’s', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/800px-McDonald%27s_Golden_Arches.svg.png') ,
-('Burger King', 'https://seeklogo.com/images/B/burger-king-new-2021-logo-F43BDE45C7-seeklogo.com.png') ,
-('Baskin Robbins', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Baskin-Robbins_logo.svg/1024px-Baskin-Robbins_logo.svg.png') ,
-('Puzata Hata', 'https://i.imgur.com/MnRaOQG.png') ,
-('Domino’s Pizza', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/2036px-Domino%27s_pizza_logo.svg.png') ,
-('KFC', 'https://i.imgur.com/iGTdJmn.png') ,
-('Sushiya', 'https://i.imgur.com/5aXABnx.png') ,
-('Papa John’s', 'https://i.imgur.com/Y1JKJZu.png') ,
-('Mister Cat', 'https://img.postershop.me/10900/Config/268261_1651117741.9328_original.png') ;
+('McDonald’s', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/800px-McDonald%27s_Golden_Arches.svg.png'),
+('Burger King', 'https://seeklogo.com/images/B/burger-king-new-2021-logo-F43BDE45C7-seeklogo.com.png'),
+('Baskin Robbins', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Baskin-Robbins_logo.svg/1024px-Baskin-Robbins_logo.svg.png'),
+('Puzata Hata', 'https://i.imgur.com/MnRaOQG.png'),
+('Domino’s Pizza', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Domino%27s_pizza_logo.svg/2036px-Domino%27s_pizza_logo.svg.png'),
+('KFC', 'https://i.imgur.com/iGTdJmn.png'),
+('Sushiya', 'https://i.imgur.com/5aXABnx.png'),
+('Papa John’s', 'https://i.imgur.com/Y1JKJZu.png'),
+('Mister Cat', 'https://img.postershop.me/10900/Config/268261_1651117741.9328_original.png');
